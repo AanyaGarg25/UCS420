eb184768fda40577561a55ee0d747c70e48ad363
AanyaGarg25 <agarg_be24@thapar.edu>
2026-08-03 09:27:40 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,140 @@
+#assignment 1
+
+# print("hello wolrd")
+
+# a=10
+# b=20
+# c=a+b
+# print(a,"+", b,"=",c)
+
+# print("Aanya")
+# print("Aanya")
+# print("Aanya")
+
+# s1="Aanya"
+# a=25
+# s2= s1+str(a)
+# print(s2)
+
+# s1="Aanya"
+# a=" Garg"
+# s2= s1+(a)
+# print(s2)
+
+
+#assignment 2.1
+# a=10
+# b=20
+# c=30
+# print(a,"+",b,"+",c,"=",a+b+c)
+
+#assignment2.2
+# s1="Hello"
+# s2=" Computer"
+# s3=" world"
+# s4=s1+s2+s3
+# print(s4)
+
+#using user input
+# s1=input("enter first string")
+# s2=input("enter second string")
+# s3=input("enter third string")
+# print(s1+s2+s3)
+
+
+# a=int(input("enter your first number: "))
+# b=int(input("enter your second number: "))
+# c=int(input("enter your third number: "))
+# print(a,"+",b,"+",c,"=",a+b+c)
+
+# for i in range(10):
+#     print(i)
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# for i in range(11):
+#     print("5","*",i,"=",5*i)
+
+#assignment 4.2
+# n=int(input("enter the number for the table : "))
+# for i in range(11):
+#     print(n,"*",i,"=",n*i)
+
+#assignment 4.3
+# n=int(input("enter the number : "))
+# sum=0
+# for i in range(n+1):
+#     sum=sum+i
+# print(sum)   
+
+
+#asssignment 5.1
+# a=int(input("enter the first number : "))
+# b=int(input("enter the second number : "))
+# if a>b:
+#     print(a,">",b)
+# else:
+#     print(a,"<",b)
+
+# a=28
+# age=21
+# if a>age:
+#     print(a,">",age)
+# else:
+#     print(a,"<",age)
+
+#asssignment 5.2
+# n=int(input("enter a number: "))
+# if n%2==0:
+#     print("the number is even ")
+# else:
+#     print("enter the number is odd ")
+
+# n=int(input("enter a number: "))
+# flag=1 #not prime 
+# for i in range(2,n):
+#     if n%i==0:
+#         print("the number is not prime")
+#         break;
+# else:
+#     print("prime")
+
+# a=int(input("enter a number: "))
+# b=int(input("enter a second number: "))
+# if a==b:
+#     print("numbers are equal")
+# elif a>=b:
+#     print(a,"Is greater")
+# else:
+#     print(b,"Is greater")
+
+#assignmnet 5.1
+# a=int(input("enter a number: "))
+# b=int(input("enter a second number: "))
+# c=int(input("enter a third  number: "))
+# maxnum=max(a,b,c)
+# print("the max number is ",maxnum)
+
+#assignment 5.2
+# n=int(input("enter the number "))
+# sum=0
+# for i in range(n+1):
+#     if i%7==0 or i%9 == 0:
+#         sum=sum+i
+# print(sum)
+
+#assignment 5.3
+# n = int(input("Enter the number: "))
+# sum = 0
+
+# for i in range(2, n + 1):
+#     for j in range(2, i):
+#         if i % j == 0:
+#             break
+#     else:
+#         sum += i
+
+# print(sum)
